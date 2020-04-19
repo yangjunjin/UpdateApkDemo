513440afe9984d275ced4c45a670a5a003155923
yjj <1172338783@qq.com>
2020-04-19 22:10:55 +0800

    下载更新添加 Notification 通知

--- app/src/main/java/com/heima/updateapkdemo/BaseDownloadTaskActivity.java
+++ app/src/main/java/com/heima/updateapkdemo/BaseDownloadTaskActivity.java
@@ -1,41 +1,22 @@
 package com.heima.updateapkdemo;
 
 import android.Manifest;
-import android.app.Activity;
-import android.app.AppOpsManager;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Binder;
 import android.os.Build;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.app.AppOpsManagerCompat;
-import androidx.core.content.ContextCompat;
-import androidx.core.content.FileProvider;
-import androidx.core.content.PermissionChecker;
 
 import com.heima.updateapkdemo.appupdate.AndroidUtil;
 import com.heima.updateapkdemo.appupdate.AppUpdateHelper;
 
-import java.io.File;
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.List;
+public class BaseDownloadTaskActivity extends AppCompatActivity {
 
-public class HelperActivity extends AppCompatActivity {
-
-    private String TAG = "HelperActivity=======";
+    private String TAG = "BaseDownloadTaskActivity=======";
     private TextView progress;
     private ProgressBar progressBar;
 
@@ -55,7 +36,7 @@ public class HelperActivity extends AppCompatActivity {
                 if (AndroidUtil.hasInstallPermission()) {
                     startDownload();
                 } else {
-                    AndroidUtil.applyInstallPermission(HelperActivity.this);
+                    AndroidUtil.applyInstallPermission(BaseDownloadTaskActivity.this);
                 }
             }
         });
@@ -65,7 +46,7 @@ public class HelperActivity extends AppCompatActivity {
 
     private void startDownload() {
         String url = "https://baicaiyouxuan.oss-cn-shenzhen.aliyuncs.com/baicaiyouxuan.apk";
-        new AppUpdateHelper(HelperActivity.this, url).setonDownLoadListener(new AppUpdateHelper.onDownLoadListener() {
+        new AppUpdateHelper(BaseDownloadTaskActivity.this, url).setonDownLoadListener(new AppUpdateHelper.onDownLoadListener() {
             @Override
             public void pending() {
                 Log.e(TAG, "pending。。。。");
@@ -84,7 +65,7 @@ public class HelperActivity extends AppCompatActivity {
                 Log.e(TAG, "completed。。。。");
                 progress.setText("apk路径=" + apkPath);
                 final String appid="com.heima.updateapkdemo";
-                AndroidUtil.installApk(HelperActivity.this,appid,apkPath);
+                AndroidUtil.installApk(BaseDownloadTaskActivity.this,appid,apkPath);
 
             }
 
@@ -110,9 +91,9 @@ public class HelperActivity extends AppCompatActivity {
                 //有这个权限
                 if (!shouldShowRequestPermissionRationale(Manifest.permission.REQUEST_INSTALL_PACKAGES)) {
                     startDownload();
-                    Toast.makeText(HelperActivity.this, "权限申请成功", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(BaseDownloadTaskActivity.this, "权限申请成功", Toast.LENGTH_SHORT).show();
                 }else{
-                    Toast.makeText(HelperActivity.this, "权限申请失败", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(BaseDownloadTaskActivity.this, "权限申请失败", Toast.LENGTH_SHORT).show();
                 }
             }
         }
